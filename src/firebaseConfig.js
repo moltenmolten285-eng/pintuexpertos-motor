5cbcf374d9f371504b1a80d5ef5b5a31a40799a5
moltenmolten285-eng <moltenmolten285@gmail.com>
2026-04-17 11:06:01 -0400

    Create firebaseConfig.js

---
+++ src/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAcyyaf8l1GHsHuBPUOdV9aJJfEVo6VfwY",
+  authDomain: "pintuexpertos-motor.firebaseapp.com",
+  projectId: "pintuexpertos-motor",
+  storageBucket: "pintuexpertos-motor.firebasestorage.app",
+  messagingSenderId: "592426682861",
+  appId: "1:592426682861:web:5304e96a2325ad68d93fe4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
