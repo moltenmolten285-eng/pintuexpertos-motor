39518a2cea0bf6750294dab3730cb84fe5481cd5
moltenmolten285-eng <moltenmolten285@gmail.com>
2026-04-17 10:52:19 -0400

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Navbar } from './components/Navbar';
+import { MotorColores } from './components/MotorColores';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <MotorColores />
+    </div>
+  );
+}
+
+export default App;
